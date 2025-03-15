9824a3abc5c4b957514925ae7bf966b679169db1
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2025-03-15 18:24:59 +0900

    Create skelton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    match args.get(0) {
+        Some(arg1) => {
+            if (arg1 == "-h") || (arg1 == "--help") {
+                print_help()
+            } else {
+                hogehoge(&arg1)
+            }
+        }
+        None => print_help(),
+    }
+}
+
+fn print_help() -> anyhow::Result<()> {
+    //hoge
+    anyhow::Ok(())
+}
+
+fn hogehoge(toml_path: &str) -> anyhow::Result<()> {
+    //hogehoge
+    anyhow::Ok(())
 }
