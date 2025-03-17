e1aa033cd5d2ec530a7dbadfca8f496355ecfe9a
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2025-03-18 02:09:58 +0900

    Move TOMLSetting->Setting

--- src/cli.rs
+++ src/cli.rs
@@ -5,89 +5,11 @@ pub struct Cli {
 }
 
 impl Cli {
-    pub fn new(toml_settings: crate::common::TOMLSettings) -> anyhow::Result<Self> {
-        let mut settings = crate::common::Settings {
-            check_interval: toml_settings.check_interval,
-            notifications: Vec::with_capacity(toml_settings.notifications.len()),
-        };
-        for notification_toml_setting in toml_settings.notifications {
-            let percentage = notification_toml_setting.percentage;
-            let Some(percentage_symbol) = percentage.chars().last() else {
-                eprintln!(
-                    "{}",
-                    format!("percentage may be empty. found:'{}'.", &percentage).red()
-                );
-                return Err(anyhow::anyhow!(
-                    "percentage may be empty. found:'{}'.",
-                    &percentage
-                ));
-            };
-            let percentage_symbol = match percentage_symbol {
-                '+' => crate::common::PercentageSymbol::Excess,
-                '-' => crate::common::PercentageSymbol::Under,
-                _ => {
-                    eprintln!(
-                        "{}",
-                        format!(
-                            "Failed to interpret '{}' as percentage symbol.",
-                            &percentage_symbol
-                        )
-                        .red()
-                    );
-                    return Err(anyhow::anyhow!(
-                        "Failed to interpret '{}' as percentage symbol.",
-                        &percentage_symbol
-                    ));
-                }
-            };
-            let percentage_int: u32 =
-                percentage[0..percentage.len() - 1]
-                    .parse()
-                    .inspect_err(|_e| {
-                        eprintln!(
-                            "{}",
-                            format!("Failed to interpret '{}' as percentage value.", &percentage)
-                                .red()
-                        );
-                    })?;
-            let power_supply: crate::battery::PowerSupply = match notification_toml_setting
-                .power_supply
-                .as_str()
-            {
-                "Adequate" => crate::battery::PowerSupply::Adequate,
-                "InAdequate" => crate::battery::PowerSupply::InAdequate,
-                "None" => crate::battery::PowerSupply::None,
-                _ => {
-                    eprintln!(
-                            "{}",
-                            format!(
-                                r#"Failed to interpret power_supply:'{}'. Use "Adequate" , "InAdequate" or "None"."#,
-                                &notification_toml_setting.power_supply
-                            )
-                            .red()
-                        );
-                    return Err(anyhow::anyhow!(
-                        r#"Failed to interpret power_supply:'{}'. Use "Adequate" , "InAdequate" or "None"."#,
-                        &notification_toml_setting.power_supply
-                    ));
-                }
-            };
-            settings
-                .notifications
-                .push(crate::common::NotificationSetting {
-                    percentage,
-                    percentage_int,
-                    percentage_symbol,
-                    power_supply,
-                    title: notification_toml_setting.title,
-                    message: notification_toml_setting.message,
-                });
-        }
-        dbg!(&settings);
+    pub fn new(settings: crate::common::Settings) -> anyhow::Result<Self> {
         Ok(Self { settings })
     }
 
-    pub fn start(&self) -> anyhow::Result<()> {
+    pub fn run(&self) -> anyhow::Result<()> {
         let duration = std::time::Duration::from_secs(self.settings.check_interval);
         loop {
             let battery_report = crate::battery::battery_check().inspect_err(|_e| {
