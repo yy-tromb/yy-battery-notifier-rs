7aa3c286eef33939b72fda72075c2db21cf0bea2
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2025-03-19 19:02:34 +0900

    Add path check and absolute in startup.rs

--- src/startup.rs
+++ src/startup.rs
@@ -1,80 +1,145 @@
 use colored::Colorize;
+use windows::core::HRESULT;
+
+use windows_registry::CURRENT_USER;
+const REG_STARTUP_KEY: &str = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+const REG_STARTUP_NAME: &str = "yy-tromb.yy-battery-notifier-rs";
+const REG_STARTUP_NAME_PCWSTR: windows::core::PCWSTR =
+    windows::core::w!("yy-tromb.yy-battery-notifier-rs");
+const REG_STARTUP_APPROVED_KEY: &str =
+    r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run";
+const REG_STARTUP_APPROVED_VALUE: [u8; 12] = [
+    0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+];
+const E_ACCESSDENIED: HRESULT = HRESULT::from_win32(0x80070005_u32);
+const E_FILENOTFOUND: HRESULT = HRESULT::from_win32(0x80070002_u32);
 
 fn register(toml_settings_path: String) -> anyhow::Result<()> {
     //use windows::ApplicationModel::{StartupTask, StartupTaskState};
     dbg!(&toml_settings_path);
+
     // registry version
-    use windows::core::HRESULT;
-    use windows_registry::CURRENT_USER;
-    const TASK_MANAGER_OVERRIDE_REGKEY: &str =
-        r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run";
-    const TASK_MANAGER_OVERRIDE_ENABLED_VALUE: [u8; 12] = [
-        0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    ];
-    const E_ACCESSDENIED: HRESULT = HRESULT::from_win32(0x80070005_u32);
-    const E_FILENOTFOUND: HRESULT = HRESULT::from_win32(0x80070002_u32);
-    let key = CURRENT_USER.create(r"Software\Microsoft\Windows\CurrentVersion\Run").inspect_err(|_e| {
-            eprintln!("{}", r"Failed to open registry 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run'.".red());
-        })?;
     let current_exe = std::env::current_exe()
         .inspect_err(|_e| eprintln!("{}", "Failed to get current exe".red()))?;
     let current_exe = current_exe.to_str().ok_or_else(|| {
-        anyhow::anyhow!("oath to current exe is empty. Unknown error may occured.")
+        anyhow::anyhow!("path to current exe is empty. Unknown error may occured.")
     })?;
-    let run_cmd = format!("{} -s {}", current_exe, toml_settings_path);
-    key.set_string(
-        "yy-tromb.yy-battery-notifier-rs",
-        &run_cmd,
-    ).inspect_err(|_e| {
-            eprintln!("{}", r"Failed to set registry 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run\yy-tromb.yy-battery-notifier-rs'.".red());
+    let run_cmd = format!(r#""{}" --gui -s "{}""#, current_exe, toml_settings_path);
+    //set
+    let key = CURRENT_USER.create(REG_STARTUP_KEY).inspect_err(|_e| {
+        eprintln!(
+            "{}",
+            format!(
+                r"Failed to open registry 'HKEY_CURRENT_USER\{}'.",
+                REG_STARTUP_KEY
+            )
+            .red()
+        );
     })?;
-    let reg_app =key.get_string("yy-tromb.yy-battery-notifier-rs").inspect_err(|_e| {
-            eprintln!("{}", r"Failed to get registry 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run\yy-tromb.yy-battery-notifier-rs'.".red());
+    key.set_string(REG_STARTUP_NAME, &run_cmd)
+        .inspect_err(|_e| {
+            eprintln!(
+                "{}",
+                format!(
+                    r"Failed to set registry 'HKEY_CURRENT_USER\{}\{}'.",
+                    REG_STARTUP_KEY, REG_STARTUP_NAME
+                )
+                .red()
+            );
+        })?;
+
+    //check
+    let reg_startup = key.get_string(REG_STARTUP_NAME).inspect_err(|_e| {
+        eprintln!(
+            "{}",
+            format!(
+                r"Failed to get registry 'HKEY_CURRENT_USER\{}\{}'.",
+                REG_STARTUP_KEY, REG_STARTUP_NAME
+            )
+            .red()
+        );
     })?;
-    if reg_app != run_cmd {
+    if reg_startup != run_cmd {
         return anyhow::Result::Err(anyhow::anyhow!(
             "{}",
             format!(
-                "Failed to set correct value to registry. found: '{}'",
-                reg_app
+                "Failed to set correct value to registry.\n Except: '{}' but Found: '{}'",
+                run_cmd, reg_startup
             )
             .red()
         ));
     }
+
     // Approve
     match CURRENT_USER
         .options()
         .write()
-        .open(TASK_MANAGER_OVERRIDE_REGKEY)
+        .create()
+        .open(REG_STARTUP_APPROVED_KEY)
     {
-        Ok(key) => key.set_bytes(
-            "yy-tromb.yy-battery-notifier-rs",
-            windows_registry::Type::Bytes,
-            &TASK_MANAGER_OVERRIDE_ENABLED_VALUE,
-        ).inspect_err(|_e| {
-            eprintln!("{}", r"Failed to set registry 'HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run\yy-tromb.yy-battery-notifier-rs'.".red());
-    })?,
+        Ok(key) => key
+            .set_bytes(
+                REG_STARTUP_NAME,
+                windows_registry::Type::Bytes,
+                &REG_STARTUP_APPROVED_VALUE,
+            )
+            .inspect_err(|_e| {
+                eprintln!(
+                    "{}",
+                    format!(
+                        r"Failed to set registry 'HKEY_CURRENT_USER\{}\{}'.",
+                        REG_STARTUP_APPROVED_KEY, REG_STARTUP_NAME
+                    )
+                    .red()
+                );
+            })?,
         Err(error) => {
-            eprintln!("{}", r"Failed to set registry 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run\yy-tromb.yy-battery-notifier-rs'. Not Found.".red());
+            eprintln!(
+                "{}",
+                format!(
+                    r"Failed to open registry 'HKEY_CURRENT_USER\{}'. Not Found.",
+                    REG_STARTUP_APPROVED_KEY
+                )
+                .red()
+            );
             return anyhow::Result::Err(error.into());
         }
     }
-    let key = CURRENT_USER.create(&TASK_MANAGER_OVERRIDE_REGKEY).inspect_err(|_e| {
-            eprintln!("{}", r"Failed to open registry 'HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run'.".red());
+
+    //approved check
+    let key = CURRENT_USER
+        .create(REG_STARTUP_APPROVED_KEY)
+        .inspect_err(|_e| {
+            eprintln!(
+                "{}",
+                format!(
+                    r"Failed to open registry 'HKEY_CURRENT_USER\{}'.",
+                    REG_STARTUP_APPROVED_KEY
+                )
+                .red()
+            );
         })?;
-    let mut reg_app_approved_value: Vec<u8> = vec![0 ; 256];
+    let mut reg_app_approved_value: Vec<u8> = vec![0; 256];
     dbg!(&reg_app_approved_value.len());
     let reg_app_approved = unsafe {
-        key.raw_get_bytes(windows::core::w!("yy-tromb.yy-battery-notifier-rs"),&mut reg_app_approved_value).inspect_err(|_e| {
-            eprintln!("{}", r"Failed to get registry 'HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run\yy-tromb.yy-battery-notifier-rs'.".red());
-        })?
+        key.raw_get_bytes(REG_STARTUP_NAME_PCWSTR, &mut reg_app_approved_value)
+            .inspect_err(|_e| {
+                eprintln!(
+                    "{}",
+                    format!(
+                        r"Failed to get registry 'HKEY_CURRENT_USER\{}\{}'.",
+                        REG_STARTUP_APPROVED_KEY, REG_STARTUP_NAME
+                    )
+                    .red()
+                );
+            })?
     };
-    if reg_app_approved.1 != TASK_MANAGER_OVERRIDE_ENABLED_VALUE {
+    if reg_app_approved.1 != REG_STARTUP_APPROVED_VALUE {
         return anyhow::Result::Err(anyhow::anyhow!(
             "{}",
             format!(
-                "Failed to set correct value to registry. found: '{}'",
-                reg_app
+                "Failed to set correct value to registry. found: '{:?}'",
+                reg_app_approved.1
             )
             .red()
         ));
@@ -84,13 +149,35 @@ fn register(toml_settings_path: String) -> anyhow::Result<()> {
 }
 
 pub fn register_cli(toml_settings_path: Option<String>, input_mode: bool) -> anyhow::Result<()> {
-    let toml_settings_path: String = match toml_settings_path {
-        Some(toml_settings_path) => anyhow::Ok(toml_settings_path),
+    use std::path::PathBuf;
+    let toml_settings_path_processed: String = match toml_settings_path {
+        Some(toml_settings_path) => {
+            let toml_settings_metadata =
+                std::fs::metadata(&toml_settings_path).inspect_err(|e| {
+                    if e.kind() == std::io::ErrorKind::NotFound {
+                        eprintln!("{}", format!("{} was not found.", toml_settings_path).red());
+                    } else {
+                        eprintln!("{}", "Unknown error.".red())
+                    }
+                })?;
+            if !toml_settings_metadata.is_file() {
+                return anyhow::Result::Err(anyhow::anyhow!(
+                    "Given '{}' is not directory.",
+                    toml_settings_path
+                ));
+            }
+            anyhow::Ok(toml_settings_path)
+        }
         None => {
             if !input_mode {
                 anyhow::Result::Err(anyhow::anyhow!(
                     "{}",
-                    "input_mode is false but not given settings.toml.".red()
+                    format!(
+                        "This is not {} But {}.",
+                        "input_mode".bold(),
+                        "not given settings.toml".bold()
+                    )
+                    .red()
                 ))
             } else {
                 let mut toml_settings_path = String::with_capacity(256);
@@ -107,17 +194,77 @@ pub fn register_cli(toml_settings_path: Option<String>, input_mode: bool) -> any
                         Err(e) => eprintln!("{}", e),
                     };
                 }
-                anyhow::Ok(toml_settings_path.trim().to_string())
+                let toml_settings_path_absolute = std::fs::canonicalize(PathBuf::from(
+                    toml_settings_path.trim(),
+                ))
+                .inspect_err(|e| {
+                    if e.kind() == std::io::ErrorKind::NotFound {
+                        eprintln!("{}", format!("{} was not found.", toml_settings_path).red());
+                    } else {
+                        eprintln!("{}", "Unknown error.".red())
+                    }
+                })?;
+                let toml_settings_path_absolute =
+                    toml_settings_path_absolute.to_str().ok_or_else(|| {
+                        anyhow::anyhow!("path to current exe is empty. Unknown error may occured.")
+                    })?;
+                println!(
+                    "Now start register. settings.toml: '{}'",
+                    toml_settings_path_absolute
+                );
+                anyhow::Ok(toml_settings_path_absolute.to_string())
             }
         }
     }?;
-    register(toml_settings_path)
+    register(toml_settings_path_processed)
 }
 
 pub fn delete() -> anyhow::Result<()> {
-    let key = CURRENT_USER.create(r"Software\Microsof
-t\Windows\CurrentVersion\Run").inspect_err(|_e| {                eprintln!("{}", r"Failed to open registry 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run'.".red());                                      })?;
-    key.remove_value(                                          "yy-tromb.yy-battery-notifier-rs").inspect_err(|_e| {                                         eprintln!("{}", r"Failed to remove  registry 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run\yy-tromb.yy-battery-notifier-rs'.".red());
+    let key = CURRENT_USER.create(REG_STARTUP_KEY).inspect_err(|_e| {
+        eprintln!(
+            "{}",
+            format!(
+                r"Failed to open registry 'HKEY_CURRENT_USER\{}'.",
+                REG_STARTUP_KEY
+            )
+            .red()
+        );
     })?;
+    key.remove_value(REG_STARTUP_NAME).inspect_err(|_e| {
+        eprintln!(
+            "{}",
+            format!(
+                r"Failed to remove registry 'HKEY_CURRENT_USER\{}\{}'.",
+                REG_STARTUP_KEY, REG_STARTUP_NAME
+            )
+            .red()
+        );
+    })?;
+    let current_exe = std::env::current_exe()
+        .inspect_err(|_e| eprintln!("{}", "Failed to get current exe".red()))?;
+    let current_exe = current_exe.to_str().ok_or_else(|| {
+        anyhow::anyhow!("oath to current exe is empty. Unknown error may occured.")
+    })?;
+    let run_cmd_part = format!("{} --gui -s ", current_exe);
+    let reg_startup = key.get_string(REG_STARTUP_NAME).inspect_err(|_e| {
+        eprintln!(
+            "{}",
+            format!(
+                r"Failed to get registry 'HKEY_CURRENT_USER\{}\{}'.",
+                REG_STARTUP_KEY, REG_STARTUP_NAME
+            )
+            .red()
+        );
+    })?;
+    if reg_startup.starts_with(&run_cmd_part) {
+        return anyhow::Result::Err(anyhow::anyhow!(
+            "{}",
+            format!(
+                "Failed to set correct value to registry.\n Except: '{}<Your >' but Found: '{}'",
+                run_cmd_part, reg_startup
+            )
+            .red()
+        ));
+    }
     anyhow::Ok(())
 }
