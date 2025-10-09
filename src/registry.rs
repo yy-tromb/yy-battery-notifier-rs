1e79bc69512e3bf96241d177f6e7bdbd6902aca9
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2025-10-09 11:18:49 +0900

    Refactoring registry::delete()

--- src/registry.rs
+++ src/registry.rs
@@ -6,6 +6,10 @@ enum RegistryValue {
     Bytes(Vec<u8>),
 }
 
+const WINDOWS_REGISTRY_ERROR_FILE_NOT_FOUND: windows::core::HRESULT =
+    windows::core::HRESULT::from_win32(0x80070002);
+
+#[inline]
 fn str_to_wide(s: &str) -> windows::core::PCWSTR {
     windows::core::PCWSTR::from_raw(s.encode_utf16().chain(std::iter::once(0)).collect::<Vec<u16>>().as_ptr())
 }
@@ -78,7 +82,7 @@ pub fn register(
     anyhow::Ok(())
 }
 
-fn check(
+fn check_registered(
     root: &windows_registry::Key,
     path: &str,
     keys_and_values: &[(&str, &RegistryValue)],
@@ -188,7 +192,113 @@ fn check(
     anyhow::Ok(())
 }
 
-pub fn register_aumid() -> anyhow::Result<()> {
+fn delete(
+    root: &windows_registry::Key,
+    parent_path: &str,
+    target_tree: &str,
+) -> anyhow::Result<()> {
+    let tree = root
+        .create(parent_path)
+        .inspect_err(|_e| {
+            if root.as_raw() == windows_registry::LOCAL_MACHINE.as_raw() {
+                eprintln!("{}", format!(r"Failed to open registry 'HKEY_LOCAL_MACHINE\{}'. You may need to run as administrator.",parent_path).red());
+            } else {
+                eprintln!("{}", format!(r"Failed to open registry 'HKEY_CURRENT_USER\{}'.",parent_path).red());
+            }
+        })?;
+        tree.remove_tree(target_tree).inspect_err(|_e| {
+            if root.as_raw() == windows_registry::LOCAL_MACHINE.as_raw() {
+                eprintln!(
+                    "{}",
+                    format!(
+                        r"Failed to remove registry key 'HKEY_LOCAL_MACHINE\{}\{}'.",
+                        parent_path, target_tree
+                    )
+                    .red()
+                );
+            } else {
+                eprintln!(
+                    "{}",
+                    format!(
+                        r"Failed to remove registry key 'HKEY_CURRENT_USER\{}\{}'.",
+                        parent_path, target_tree
+                    )
+                    .red()
+                );
+            }
+        })?;
+    anyhow::Ok(())
+}
+
+fn check_deleted(
+    root: &windows_registry::Key,
+    path: &str,
+    keys: &[&str],
+) -> anyhow::Result<()> {
+    let tree = root
+        .create(path)
+        .inspect_err(|_e| {
+            if root.as_raw() == windows_registry::LOCAL_MACHINE.as_raw() {
+                eprintln!("{}", format!(r"Failed to open registry 'HKEY_LOCAL_MACHINE\{}'. You may need to run as administrator.",path).red());
+            } else {
+                eprintln!("{}", format!(r"Failed to open registry 'HKEY_CURRENT_USER\{}'.",path).red());
+            }
+        })?;
+    for key in keys {
+        match tree.get_value(key) {
+            Ok(v) => {
+                if root.as_raw() == windows_registry::LOCAL_MACHINE.as_raw() {
+                    return anyhow::Result::Err(anyhow::anyhow!(
+                        "{}",
+                        format!(
+                            r"Failed to delete registry 'HKEY_LOCAL_MACHINE\{}\{}'.\n Found: {:?}",
+                            path, key, v
+                        )
+                        .red()
+                    ));
+                } else {
+                    return anyhow::Result::Err(anyhow::anyhow!(
+                        "{}",
+                        format!(
+                            r"Failed to read registry 'HKEY_CURRENT_USER\{}\{}'.\n Found: {:?}",
+                            path, key, v
+                        )
+                        .red()
+                    ));
+                }
+            }
+            Err(e) => {
+                if e.code() == WINDOWS_REGISTRY_ERROR_FILE_NOT_FOUND {
+                    // do nothing
+                } else {
+                    if root.as_raw() == windows_registry::LOCAL_MACHINE.as_raw() {
+                        eprintln!(
+                            "{}",
+                            format!(
+                                r"Failed to read registry 'HKEY_LOCAL_MACHINE\{}\{}'.",
+                                path, key
+                            )
+                            .red()
+                        );
+                    } else {
+                        eprintln!(
+                            "{}",
+                            format!(
+                                r"Failed to read registry 'HKEY_CURRENT_USER\{}\{}'.",
+                                path, key
+                            )
+                            .red()
+                        );
+                    }
+                    return anyhow::Result::Err(anyhow::Error::from(e));
+                }
+            }
+        }
+    }
+    anyhow::Ok(())
+}
+
+pub fn register_and_check_aumid() -> anyhow::Result<()> {
     let key = CURRENT_USER
         .create(r"SOFTWARE\Classes\AppUserModelId\yy-tromb.yy-battery-notifier-rs")
         .inspect_err(|_e| {
@@ -221,7 +331,7 @@ pub fn register_aumid() -> anyhow::Result<()> {
     }
 }
 
-pub fn delete_aumid() -> anyhow::Result<()> {
+pub fn delete_and_check_aumid() -> anyhow::Result<()> {
     let key = CURRENT_USER
         .create(r"SOFTWARE\Classes\AppUserModelId")
         .inspect_err(|_e| {
