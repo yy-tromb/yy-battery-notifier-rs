2ab05f9e358552fffc172953c2aa2e1ca2a35c43
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2025-10-10 16:04:22 +0900

    Add progress bar in notifications

--- src/notify.rs
+++ src/notify.rs
@@ -12,10 +12,19 @@ fn notify_winrt_toast(
     title: &str,
     message: &str,
 ) -> anyhow::Result<()> {
-    use tauri_winrt_notification::{Duration, Toast};
+    use tauri_winrt_notification::{Duration, Progress, Toast};
+    let progress = Progress {
+        tag: "tag".to_string(),
+        title: "Now Battery Level:".to_string(),
+        status: format!("{}:{}:{} left", battery_report.remaining_seconds / 3600, (battery_report.remaining_seconds % 3600) / 60, battery_report.remaining_seconds % 60),
+        value: battery_report.percentage as f32 / 100.0,
+        value_string: format!("{}%", battery_report.percentage),
+    };
     Toast::new("yy-tromb.yy-battery-notifier-rs")
         .title(title)
         .text1(message)
+        .progress(&progress)
+        .duration(Duration::Short)
         .show()
         .map_err(|error| match error {
             tauri_winrt_notification::Error::Os(e) => anyhow::Error::from(e),
