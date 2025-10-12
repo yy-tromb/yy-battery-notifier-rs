eface5311b4cbf79687523dc5b8d51fca7f381b2
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2025-10-12 18:59:12 +0900

    Add battery check from win32 api

--- src/battery.rs
+++ src/battery.rs
@@ -24,9 +24,20 @@ impl From<windows::System::Power::PowerSupplyStatus> for PowerSupply {
     }
 }
 
-#[inline]
 pub fn battery_check() -> anyhow::Result<BatteryReport> {
-    battery_check_winrt()
+    let mut battery_report = battery_check_winrt()?;
+    dbg!(&battery_report);
+    // tmporary
+    if battery_report.remaining_seconds.is_none() {
+        let batttery_report_win32 = battery_check_win32()?;
+        dbg!(&batttery_report_win32);
+        if batttery_report_win32.remaining_seconds.is_some() {
+            battery_report.remaining_seconds = batttery_report_win32.remaining_seconds;
+        }
+        Ok(battery_report)
+    } else {
+        Ok(battery_report)
+    }
 }
 
 fn battery_check_winrt() -> anyhow::Result<BatteryReport> {
@@ -42,6 +53,31 @@ fn battery_check_winrt() -> anyhow::Result<BatteryReport> {
     };
     let power_supply: PowerSupply = PowerManager::PowerSupplyStatus()?.into();
     // do something
+    Ok(BatteryReport {
+        percentage,
+        remaining_seconds,
+        power_supply,
+    })
+}
+
+fn battery_check_win32() -> anyhow::Result<BatteryReport> {
+    use windows::Win32::System::Power::{GetSystemPowerStatus, SYSTEM_POWER_STATUS};
+    let mut system_power_status = SYSTEM_POWER_STATUS::default();
+    unsafe {
+        GetSystemPowerStatus(&mut system_power_status)?;
+    }
+    let percentage = system_power_status.BatteryLifePercent as u32;
+    let remaining_seconds = if system_power_status.BatteryLifeTime == u32::MAX {
+        None
+    } else {
+        Some(system_power_status.BatteryLifeTime as u64)
+    };
+    let power_supply = match system_power_status.ACLineStatus {
+        0 => PowerSupply::None,
+        1 => PowerSupply::Adequate,
+        255 => PowerSupply::InAdequate,
+        _ => unreachable!(),
+    };
     Ok(BatteryReport {
         percentage,
         remaining_seconds,
